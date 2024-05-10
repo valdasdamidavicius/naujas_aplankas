e9a103e609a409d74a084e71fcbe8ba487ba02a0
valdasdamidavicius <valdukasirko@gmail.com>
2024-05-10 11:46:25 +0300

    akordeonas

---
+++ akordeonas/a.js
@@ -0,0 +1,16 @@
+const acc = document.querySelectorAll('.acordeon');
+
+
+for(let i = 0; i < acc.length; i++) {
+    acc[i].addEventListener('click', function() {
+        this.classList.toggle('active')
+        const desc = this.nextElementSibling;
+
+        if (desc.style.maxHeight) {
+            desc.style.maxHeight = null
+        }
+        else {
+            desc.style.maxHeight = desc.scrollHeight + "px"
+        }
+    });
+}
